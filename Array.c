683b803673feea810fdd0c1deab9ae8fee2f921e
MAZHAR1807102 <imazharul175@gmail.com>
2023-10-20 00:34:50 +0600

    Second day

---
+++ Array.c
@@ -0,0 +1,13 @@
+//single dimentional array
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[]= {10, 0, 20, 0, 30}; //creating and initializing array
+    //traversing array
+    cout << a[2] <<endl;
+  /*  for (int i = 0; i < 5; i++)
+    {
+        cout<<arr[i]<<"\n";
+    }*/
+}
---
+++ Array.cpp
@@ -0,0 +1,29 @@
+
+//single dimentional array
+#include<iostream>
+#include<string>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    //int arr[]= {10, 50, 20, 0, 30};
+    int arr[10];
+    int i;
+ /*   for(i = 0; i < 5; i++){
+
+  //  scanf("%d", &arr[i]);
+    //printf("%d\n", i);
+    }
+    //creating and initializing array
+    //traversing array
+    //cout << arr[2] <<endl;
+    i = 0;
+    for (i = 0; i < 5; i++)
+    {
+   //     cout<<arr[i]<<"\n";
+        //printf("%d\n", i);
+    }*/
+    string st("LIMON");
+    cout << st<<endl;
+    printf("%s\n", st.cs_str());
+}
